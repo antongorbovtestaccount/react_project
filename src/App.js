d4765f2bcb94475f8ad682985c6829701df5337b
antongorbovtestaccount <anton_gorbov@hotmail.com>
2021-08-15 22:20:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Project Setup
+          Test Project Setup 2
         </p>
         <a
           className="App-link"
